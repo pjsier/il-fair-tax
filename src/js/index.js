db8589590985005d89d05a953c78d80918e91e7f
pjsier <pjsier@gmail.com>
2020-08-17 12:43:54 -0500

    fix: safari form.elements issue

--- src/js/index.js
+++ src/js/index.js
@@ -31,7 +31,9 @@ function formToObj(form) {
   const formNames = [
     ...new Set(
       Object.values(form.elements)
-        .map((input) => input.getAttribute("name"))
+        .map((input) =>
+          (input instanceof NodeList ? input[0] : input).getAttribute("name")
+        )
         .filter((name) => !!name)
     ),
   ]
